e2ab9c5cd3af9789063e4a248e8880b0614de585
Lucas Nominato <165806745+LucasNominato2004@users.noreply.github.com>
2024-08-30 00:46:37 -0300

    Update Principal.java

--- maven.demo/src/main/java/app/Principal.java
+++ maven.demo/src/main/java/app/Principal.java
@@ -1,7 +1,7 @@
 package app;
 
 import java.util.List;
-
+import java.util.Scanner;
 import dao.DAO;
 import dao.UsuarioDAO;
 import model.Usuario;
@@ -9,36 +9,39 @@ import model.Usuario;
 public class Aplicacao {
 	
 	public static void main(String[] args) throws Exception {
-		
-		UsuarioDAO usuarioDAO = new UsuarioDAO();
-
-    System.out.println("\n\n==== Mostrar usuários ordenados por login === ");
-		usuarios = usuarioDAO.getOrderByLogin();
-		for (Usuario u: usuarios) {
-			System.out.println(u.toString());
-		}
-
-    System.out.println("\n\n==== Mostrar usuários ordenados por código === ");
-		usuarios = usuarioDAO.getOrderByCodigo();
-		for (Usuario u: usuarios) {
-			System.out.println(u.toString());
-		}
-		
-		System.out.println("\n\n==== Inserir usuário === ");
-		Usuario usuario = new Usuario(11, "pablo", "pablo",'M');
-		if(usuarioDAO.insert(usuario) == true) {
-			System.out.println("Inserção com sucesso -> " + usuario.toString());
-		}
-		
-		System.out.println("\n\n==== Excluir usuário (código " + usuario.getCodigo() + ") === ");
-		usuarioDAO.delete(usuario.getCodigo());
-
-    System.out.println("\n\n==== Atualizar senha (código (" + usuario.getCodigo() + ") === ");
-		usuario.setSenha(DAO.toMD5("pablo"));
-		usuarioDAO.update(usuario);
-
-    System.out.println("\n\n==== Sair do Programa === ");
-    break;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Digite a opcao que deseja selecionar [1- Listar, 2- Inserir, 3- Excluir, 4- Atualizar, 5- Sair]");
+		int opcao = scanner.nextInt();
+		do{
+			switch(opcao)
+			        case 1:  
+					usuarios = usuarioDAO.getOrderByCodigo();
+				        for (Usuario u: usuarios) {
+						System.out.println(u.toString());
+					}
+              			break;
+			        case 2:  
+              				System.out.println("\n\n==== Inserir === ");
+					Usuario usuario = new Usuario(11, "pablo", "pablo",'M');
+					if(usuarioDAO.insert(usuario) == true) {
+						System.out.println("Inserção com sucesso -> " + usuario.toString());
+					}
+              			break;
+				case 3:  
+              				System.out.println("\n\n==== Excluir (código " + usuario.getCodigo() + ") === ");
+					usuarioDAO.delete(usuario.getCodigo());
+              			break;
+				case 4:  
+              				System.out.println("\n\n==== Atualizar senha (código (" + usuario.getCodigo() + ") === ");
+					usuario.setSenha(DAO.toMD5("pablo"));
+					usuarioDAO.update(usuario);
+              			break;
+				case 5:  
+              				System.out.println("\n\n==== Saindo === ");
+              			break;				
+			
+		}while(opcao != 5);
+		scanner.close();
 	}
 }
 
