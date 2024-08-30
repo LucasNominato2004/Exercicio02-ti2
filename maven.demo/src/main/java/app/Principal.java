50d7120ec176aba7f88befafc869f404dd012a04
Lucas Nominato <165806745+LucasNominato2004@users.noreply.github.com>
2024-08-30 00:31:57 -0300

    Update Principal.java

--- maven.demo/src/main/java/app/Principal.java
+++ maven.demo/src/main/java/app/Principal.java
@@ -1,4 +1,45 @@
+package app;
 
-public class Principal {
+import java.util.List;
 
+import dao.DAO;
+import dao.UsuarioDAO;
+import model.Usuario;
+
+public class Aplicacao {
+	
+	public static void main(String[] args) throws Exception {
+		
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+
+    System.out.println("\n\n==== Mostrar usuários ordenados por login === ");
+		usuarios = usuarioDAO.getOrderByLogin();
+		for (Usuario u: usuarios) {
+			System.out.println(u.toString());
+		}
+
+    System.out.println("\n\n==== Mostrar usuários ordenados por código === ");
+		usuarios = usuarioDAO.getOrderByCodigo();
+		for (Usuario u: usuarios) {
+			System.out.println(u.toString());
+		}
+		
+		System.out.println("\n\n==== Inserir usuário === ");
+		Usuario usuario = new Usuario(11, "pablo", "pablo",'M');
+		if(usuarioDAO.insert(usuario) == true) {
+			System.out.println("Inserção com sucesso -> " + usuario.toString());
+		}
+		
+		System.out.println("\n\n==== Excluir usuário (código " + usuario.getCodigo() + ") === ");
+		usuarioDAO.delete(usuario.getCodigo());
+
+    System.out.println("\n\n==== Atualizar senha (código (" + usuario.getCodigo() + ") === ");
+		usuario.setSenha(DAO.toMD5("pablo"));
+		usuarioDAO.update(usuario);
+
+    System.out.println("\n\n==== Sair do Programa === ");
+    break;
+	}
 }
+
+//Listar inserir exluir atualizar sair
